1fe21e06bca8863632999db8bcbc3271fa8e4ac2
Akash <64420718+debakash@users.noreply.github.com>
2021-06-26 21:18:45 +0100

    Removed everything login related as im fussy af.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 const router = Router()
 
-import login from "./login/index";
-router.use('/login', login)
-
+router.get("/", (req, res) => res.render("index"))
 
 export default router;
