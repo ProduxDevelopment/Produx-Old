4be909a7a6bc5b7f30d136f2dc4351434549f7e0
Akash <64420718+debakash@users.noreply.github.com>
2021-07-09 23:03:11 +0100

    more themes

--- src/routes/login/routes/register.ts
+++ src/routes/login/routes/register.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 const router = Router()
 
+import config from "../../../../Config/config.json"
+import meta from "../../../../Config/meta.json"
+import theme from "../../../../Config/theme.json"
+
 router.get("/", (req, res) => {
     if(req.isAuthenticated()) return res.redirect("/panel")
-    res.render("register")
+    res.render("register", {
+        title: "Login",
+        description: meta.pageDescription,
+        author: meta.author,
+        keywords: meta.keywords,
+        siteName: config.customisation.info.siteName,
+        siteColour: "orange",
+        data: theme
+    })
 })
 
 export default router;
