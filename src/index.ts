7addac48865dbbad974845adfac18a739610ae7b
Akash <64420718+debakash@users.noreply.github.com>
2021-08-01 14:27:32 +0100

    stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Config from "../Config/config.json";
 import { config } from "./interfaces/config";
 
-import { ExpressClient } from "./Client/ExpressClient"
-import { DatabaseClient } from "./Client/DatabaseClient"
-import { PayPalClient } from "./Client/PaypalClient";
+import { ExpressClient } from "./classes/ExpressClient"
+import { DatabaseClient } from "./classes/DatabaseClient"
+import { PayPalClient } from "./classes/PaypalClient";
 
 const express = new ExpressClient(Config as config)
 const database = new DatabaseClient(Config as config)
